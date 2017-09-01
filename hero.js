18507511494d57fea62bc5ae41e5390415750f21
Travis Grimmond <travisslack1994@outlook.com>
2017-09-01 11:25:18 +0100

    Made a specs file with hero_spec in it with a hero file also

---
+++ hero.js
@@ -0,0 +1,5 @@
+var Hero = function(name, favouriteFood) {
+  this.name = name;
+  this.favouriteFood = favouriteFood;
+  this.health = 100;
+}
---
+++ specs/hero_spec.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var Hero = require("../hero.js");
+
+describe("Hero", function() {
+
+  var hero;
+
+  beforeEach(function{
+    hero = new Hero("Batman", "Steak", 100);
+  })
+
+  
+
+
+
+
+
+
+
+})
