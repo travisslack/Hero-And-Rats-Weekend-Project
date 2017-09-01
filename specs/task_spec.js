1ca276e0e5e0a1bb3237f530a1a6c5db055946c7
Travis Grimmond <travisslack1994@outlook.com>
2017-09-01 13:31:37 +0100

    Task class made with tests

---
+++ specs/task_spec.js
@@ -0,0 +1,23 @@
+var assert = require("assert");
+var Task = require("../task.js")
+
+describe ("Task", function() {
+
+  var Task
+
+  beforeEach(function() {
+    task1 = new Task(3, 2, 25);
+  })
+
+  // it("Task has a difficulty level", function() {
+  //   assert.strictEqual
+  // })
+
+
+
+
+
+
+
+
+})
---
+++ task.js
@@ -0,0 +1,13 @@
+var Task = function(difficultyLevel, urgencyLevel, reward); {
+  this.difficultyLevel = difficultyLevel;
+  this.urgencyLevel = urgencyLevel;
+  this.reward = reward;
+
+
+
+
+}
+
+
+
+module.exports = Task;
